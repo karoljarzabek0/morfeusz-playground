909a30e5ed5c89b1745d1749a14d8c74b15aa762
karoljarzabek0 <jarzabek40@gmail.com>
2026-02-05 09:10:24 +0000

    fucntion for unkonw words

--- main.cpp
+++ main.cpp
@@ -4,7 +4,28 @@
 
 std::set<std::string> detectUnknownWords(morfeusz::Morfeusz *m, std::string text) {
     // TODO
-} 
+    std::set<std::string> unknownWords;
+
+    std::vector<morfeusz::MorphInterpretation> r;
+    m->analyse(text, r);
+    for (const auto &i : r) {
+        if (i.isIgn()) {
+            unknownWords.insert(i.orth);
+        }
+    }
+    return unknownWords;
+}
+
+void printResults(morfeusz::Morfeusz *m, std::string text) {
+    std::vector<morfeusz::MorphInterpretation> r;
+    m->analyse(text, r);
+
+    for (const auto &i : r) {
+        std::cout << "----------------" << std::endl;
+        std::cout << i.orth << " -> ";
+        std::cout << i.lemma << std::endl;
+    }
+}
 
 
 int main() {
@@ -13,12 +34,11 @@ int main() {
     morfeusz::Morfeusz *m = morfeusz::Morfeusz::createInstance();
     m->setCharset(morfeusz::UTF8);
 
-    std::vector<morfeusz::MorphInterpretation> r;
-    m->analyse("Załóż gąbkę na klawesyn.", r);
-
-    for (const auto &i : r) {
-        std::cout << "----------------" << std::endl;
-        std::cout << i.orth << " -> ";
-        std::cout << i.lemma << std::endl;
+    auto results = detectUnknownWords(m, "Dane niekliniczne, uzyskane na podstawie konwencjonalnych badań farmakologicznych dotyczących bezpieczeństwa stosowania, toksyczności po podaniu wielokrotnym, genotoksyczności i");
+    
+    for (const auto &i : results) {
+        std::cout << i << " ";
+        std::cout << std::endl;
     }
+
 }
